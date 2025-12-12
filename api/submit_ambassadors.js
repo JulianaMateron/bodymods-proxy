d607fc81b7e7802d9da9ee6023df12dc96230769
Juliana Materon <juliana@bodymods.ca>
2025-12-12 11:42:50 -0800

    Update ambassador form - confirmation email

--- api/submit_ambassadors.js
+++ api/submit_ambassadors.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
         }
 
       // 3. If reCAPTCHA passes, forward to Google Apps Script
-      const response = await fetch('https://script.google.com/macros/s/AKfycbyUTlPPjugJc6VO3gT4jJDvBMLyvma8dc2MTnTc7iSq9Jt77p8Ub1z4GpFrfXFZ9v7drw/exec', {
+      const response = await fetch('https://script.google.com/macros/s/AKfycbwsxm-Lu1KDBoGLAD-yB-QkK7EgiE4wfkTV2c5N83IysG6hFyDaiKCjv88pvz9DBsIlnA/exec', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
